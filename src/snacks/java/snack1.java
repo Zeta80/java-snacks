4a9b0f9c1feade651bf5adf18798b284e8aebb74
Mirko Robbi <mirko.robbi@gmail.com>
2023-03-09 14:12:17 +0100

    First Snack

---
+++ src/snacks/java/snack1.java
@@ -0,0 +1,19 @@
+package snacks.java;
+
+import java.util.Scanner;
+
+public class snack1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Inserisci un numero: ");
+        int numero = scan.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("Il numero inserito è pari: " + numero);
+        } else {
+            System.out.println("Il numero inserito è dispari: " + numero);
+            System.out.println("Il numero successivo è: " + (numero + 1));
+        }
+
+    }
+}
